a7a554d14cdb3dbe1cf62f705d3bdf6360daa8ec
Beyam <160472943+shikileliondor@users.noreply.github.com>
2026-04-23 20:24:42 +0000

    feat(home): add animated intro screen and homepage transition

--- resources/js/Pages/Home/Index.jsx
+++ resources/js/Pages/Home/Index.jsx
@@ -1,26 +1,12 @@
 import { Head } from '@inertiajs/react';
-import { useState } from 'react';
 
-import IntroSplash from '@/components/IntroSplash';
+import App from '@/Pages/Home/App';
 
 export default function HomeIndex() {
-    const [showIntro, setShowIntro] = useState(true);
-
     return (
         <>
             <Head title="Accueil" />
-
-            <main className="relative min-h-screen w-full overflow-hidden bg-black">
-                <IntroSplash show={showIntro} onComplete={() => setShowIntro(false)} />
-
-                {/* <section className="h-screen w-full">
-                    <img
-                        src="/images/banniere.png"
-                        alt="Bannière Light Foursquare"
-                        className="h-full w-full object-cover object-center"
-                    />
-                </section> */}
-            </main>
+            <App />
         </>
     );
 }
